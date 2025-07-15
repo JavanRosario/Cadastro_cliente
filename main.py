0a9e2bee0e9e1c5600c168d05ecbb7f2bf7dca24
JavanRosario <oliveiraajavan@hotmail.com>
2025-07-15 19:06:57 -0300

    modularização e organização inicial

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from pathlib import Path
 from openpyxl import workbook
 
 ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
+ctk.set_default_color_theme("dark-blue")
 
 
 class App(ctk.CTk):
     def __init__(self):
-        super().__init__()  
+        super().__init__()
         self.layout_config()
         self.appearance()
         self.all_system()
@@ -44,7 +44,7 @@ class App(ctk.CTk):
 
         # header frame title
         frame = ctk.CTkFrame(self, width=800, height=50,
-                             corner_radius=0, bg_color="green", fg_color="green")
+                             corner_radius=0, bg_color="green", fg_color="gray")
         frame.place(x=0, y=10)
         title = ctk.CTkLabel(frame, text='Sistema Cadastro de Clientes', font=(
             "Segoe UI", 25), text_color="#fff")
@@ -53,31 +53,72 @@ class App(ctk.CTk):
         small_box = ctk.CTkLabel(self, text='Prencha todos os campos:', font=(
             "Segoe UI", 16), text_color=["#000", "#fff"]).place(x=50, y=70)
 
-        def submit():
+        file = Path('Clientes.xlsx')
+        if file.exists():
             pass
+        else:
+            file= Workbook()
+            file = file.active
+            sheet['A1'] = 'Nome completo'
+            sheet['B2'] = 'Contato'
+            sheet['C3'] = 'Idade'
+            sheet['D4'] = 'Gênero'
+            sheet['E1'] = 'Endereço'
+            sheet['F1'] = 'Observações'
+
+            file.save('Clientes.xlsx')
+
+        def submit():
+            name = name_value.get()
+            contact = contact_value.get()
+            age = age_value.get()
+            adress = address_value.get()
+            gender = gender_combobox.get()
+            obs = obs_entry.get(0.0, END)
+
+            file = openpyxl.load_workbook('Clientes.xlsx')
+            sheet = file.active
+            sheet.cell(column=1, row=sheet.max_row+1, value=name)
+            sheet.cell(column=2, row=sheet.max_row, value=contact)
+            sheet.cell(column=3, row=sheet.max_row, value=age)
+            sheet.cell(column=4, row=sheet.max_row, value=adress)
+            sheet.cell(column=5, row=sheet.max_row, value=gender)
+            sheet.cell(column=6, row=sheet.max_row, value=obs)
+
+            file.save(r'Clientes.xlsx')
+            messagebox.showinfo('Systema', 'Dados santos com sucesso!')
 
         def clear():
-            pass
+            name_value.set('')
+            contact_value.set('')
+            age_value.set('')
+            address_value.set('')
+            obs_entry.delete("0.0", END)
+
+        name_value = StringVar()
+        contact_value = StringVar()
+        age_value = StringVar()
+        address_value = StringVar()
 
         # name
         input_name = ctk.CTkLabel(self, text='Nome Completo:', font=(
             "Segoe UI", 16), text_color=["#000", "#fff"])
         input_name.place(x=x_label, y=y_start)
-        name_entry = ctk.CTkEntry(self, width=500, font=(
+        name_entry = ctk.CTkEntry(self, width=500, textvariable=name_value, font=(
             "Segoe UI", 16), fg_color='transparent')
         name_entry.place(x=x_entry, y=y_start-5)
         # contact
         input_contact = ctk.CTkLabel(self, text='Número de contato:', font=(
             "Segoe UI", 16), text_color=["#000", "#fff"])
         input_contact.place(x=x_label, y=y_start + y_step)
-        contact_entry = ctk.CTkEntry(self, width=200, font=(
+        contact_entry = ctk.CTkEntry(self, width=200, textvariable=contact_value, font=(
             "Segoe UI", 16), fg_color='transparent')
         contact_entry.place(x=x_entry, y=y_start + y_step - 5)
         # age
         input_age = ctk.CTkLabel(self, text='Idade:', font=(
             "Segoe UI", 16), text_color=["#000", "#fff"])
         input_age.place(x=440, y=y_start + y_step)
-        age_entry = ctk.CTkEntry(self, width=80, font=(
+        age_entry = ctk.CTkEntry(self, width=80,  textvariable=age_value, font=(
             "Segoe UI", 16), fg_color='transparent')
         age_entry.place(x=500, y=y_start + y_step - 5)
         # gender
@@ -85,14 +126,14 @@ class App(ctk.CTk):
             "Segoe UI", 16), text_color=["#000", "#fff"])
         input_gender.place(x=600, y=y_start + y_step)
         gender_combobox = ctk.CTkComboBox(
-            self, values=['Masculino', 'Feminino'], font=('Segoe UI', 15), width=120)
+            self, values=['Masculino', 'Feminino'], font=('Segoe UI', 15), width=120,)
         gender_combobox.set('Masculino')
         gender_combobox.place(x=670, y=y_start + y_step - 5)
         # address
         input_address = ctk.CTkLabel(self, text='Endereço:', font=(
             "Segoe UI", 16), text_color=["#000", "#fff"])
         input_address.place(x=x_label, y=y_start + 2 * y_step)
-        address_entry = ctk.CTkEntry(self, width=500, font=(
+        address_entry = ctk.CTkEntry(self, width=500, textvariable=address_value, font=(
             "Segoe UI", 16), fg_color='transparent')
         address_entry.place(x=x_entry, y=y_start + 2 * y_step - 5)
         # OBS
